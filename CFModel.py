0c1270ebe488cf61a53768d7cc673819f3ea932b
James Le <le_j6@denison.edu>
2018-04-15 22:13:18 +0700

    SVD Model In Progress

--- CFModel.py
+++ CFModel.py
@@ -1,20 +1,32 @@
 # A simple implementation of matrix factorization for collaborative filtering expressed as a Keras Sequential model
 
+# Keras uses TensorFlow tensor library as the backend system to do the heavy compiling
+
 import numpy as np
-from keras.layers import Embedding, Reshape, Merge, Dropout, Dense
+from keras.layers import Embedding, Reshape, Merge
 from keras.models import Sequential
 
 class CFModel(Sequential):
 
+    # The constructor for the class
     def __init__(self, n_users, m_items, k_factors, **kwargs):
+        # P is the embedding layer that creates an User by latent factors matrix.
+        # If the intput is a user_id, P returns the latent factor vector for that user.
         P = Sequential()
         P.add(Embedding(n_users, k_factors, input_length=1))
         P.add(Reshape((k_factors,)))
+
+        # Q is the embedding layer that creates a Movie by latent factors matrix.
+        # If the input is a movie_id, Q returns the latent factor vector for that movie.
         Q = Sequential()
         Q.add(Embedding(m_items, k_factors, input_length=1))
         Q.add(Reshape((k_factors,)))
+
         super(CFModel, self).__init__(**kwargs)
+        
+        # The Merge layer takes the dot product of user and movie latent factor vectors to return the corresponding rating.
         self.add(Merge([P, Q], mode='dot', dot_axes=1))
 
+    # The rate function to predict user's rating of unrated items
     def rate(self, user_id, item_id):
         return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
